6e74b704ce0a7ee10f73f011657d015d9824c14b
ddragonok <ddragon600@gmail.com>
2021-02-19 13:56:04 +0300

    Function decomposition

--- 4_part/1_lab.py
+++ 4_part/1_lab.py
@@ -17,10 +17,17 @@ first = (a/(h*h)) + b/h
 second = ((1/t) - ((2*a)/(h*h)) - (b/h) + c)
 third = a/(h*h)
 
-u = np.zeros((N + 1, K + 1))  # Finite-difference mesh
+u_mesh = np.zeros((N + 1, K + 1))  # Finite-difference mesh
 
 
-def create_u():
+def draw_u(u):
+    x = [h * i for i in range(N + 1)]
+    for i in range(K + 1):  # Graph of each time layer
+        plt.plot(x, u[:, i])
+    plt.show()
+
+
+def create_u(u):
     for j in range(N + 1):  # Initial condition
         u[j][0] = math.sin(j*h)
 
@@ -33,11 +40,7 @@ def create_u():
         u[N][k] = (u[N-1][k] * (1/h) - math.exp((c - a) * k*t) * (math.cos(b * k*t) + math.sin(b * k*t))) / ((1/h) + 1)
 
     print(np.around(u, 3))
-
-    x = [h * i for i in range(N + 1)]
-    for i in range(K + 1):  # Graph of each time layer
-        plt.plot(x, u[:, i])
-    plt.show()
+    draw_u(u)
 
 
-create_u()
+create_u(u_mesh)
